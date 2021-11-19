cafc2f888a67c33c0a2ed5680e87ba5e0a3b35f6
iabhishek15 <158singhabhi@gmail.com>
2021-11-19 11:13:30 +0530

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'shielded-dusk-12394.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'app_with_pgs.herokuapp.com' }
 end
