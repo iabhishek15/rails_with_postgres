14e57530f0ce8bf33125c6d264c27bc29979a93d
iabhishek15 <158singhabhi@gmail.com>
2021-11-21 15:26:45 +0530

    updated my url in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'aqueous-sands-45740.herokuapp.com', protocol: "https" }
+  config.action_mailer.default_url_options = { host: 'shielded-dusk-12394.herokuapp.com', protocol: "https" }
   
 
   #******************mailer in action below
